79d4036434ce8ebaedbbf2a95705d40ff6cff8e9
DipanshuOjha <143347086+DipanshuOjha@users.noreply.github.com>
2025-03-16 13:43:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require("cors");
-const router = require('../paymentapp-backend/routes/index1');
+const router = require('./routes/index1');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
